ffbe54d2b69ba6d57970879e4006176bd710ef25
Danrejk <SDanrejk@gmail.com>
2024-04-01 01:27:07 +0200

    unwatch-role

--- databaseTest.js
+++ databaseTest.js
@@ -4,8 +4,8 @@ const db = new sqlite3.Database('./data.db', sqlite3.OPEN_READWRITE, (err) =>{
   console.log('database connection successful')
 })
 
-// db.run(`CREATE TABLE watchedRoles(roleId, guildId)`);
-db.run(`DELETE FROM watchedRoles`)
+//db.run(`CREATE TABLE updatedMessages(channelId, messageId)`);
+//db.run(`DELETE FROM watchedRoles`)
 
 db.close((err) => {
   if(err) return console.error(err.message)
