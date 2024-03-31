e1b76ca7c217f5af3a0775e8b09b0fc34d7e0d6d
Danrejk <SDanrejk@gmail.com>
2024-03-31 21:58:50 +0200

    started work on checkStatus

--- commands/createStatus.js
+++ commands/createStatus.js
@@ -1,14 +1,17 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { PermissionFlagsBits } = require("discord-api-types/v10");
 
+const sqlite3 = require('sqlite3').verbose();
+
 module.exports = {
 	data: new SlashCommandBuilder()
         .setName("create-status")
         .setDescription("Creates a message showing the current count of players with watched roles.")
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
 	execute: async ({ client, interaction }) => {
-        await interaction.reply({
-          content:"lorem ipsum",
-        })
+                const db = new sqlite3.Database('./data.db', sqlite3.OPEN_READWRITE, (err) =>{
+                        if (err) return console.errror(err.message);})
+
+                db.close((err) => { if(err) return console.error(err.message) })
 	},
 }
