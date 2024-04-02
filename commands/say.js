620fe6ff7d50a4d4ee945ca5f819473af09b4309
Danrejk <SDanrejk@gmail.com>
2024-04-02 17:27:33 +0200

    added say

---
+++ commands/say.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { PermissionFlagsBits } = require("discord-api-types/v10");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+        .setName("say")
+        .setDescription("Nie dla psa kiełbasa")
+        .addStringOption(option =>
+            option.setName("tresc").setDescription("co mam wówić złotko").setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
+	execute: async ({ client, interaction }) => {
+        if (interaction.user.id === '512664079496642575'){
+                let tresc = interaction.options.getString("tresc")
+                await interaction.channel.send({content: tresc});
+        }
+        else{
+                await interaction.reply({content:"Bredzisz głupoty. Nie powiem tego.", ephemeral: true});
+        }
+	},
+}
