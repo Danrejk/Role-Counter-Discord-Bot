709a35243cb191715c83764d8f1fbf6399bf8cb5
Danrejk <SDanrejk@gmail.com>
2024-05-07 17:01:01 +0200

    Fixed crash error when responding to a message wrong

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { error } = require("console");
 const { PermissionFlagsBits } = require("discord-api-types/v10");
 
 module.exports = {
@@ -17,11 +18,16 @@ module.exports = {
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
 	execute: async ({ client, interaction }) => {
                 let message = interaction.options.getString("message")
-                await interaction.channel.send({
-                    content: message,
-                    tts: interaction.options.getBoolean("tts") ?? false,
-                    reply: { messageReference: interaction.options.getString("reply") ?? null }
-                });
+                try{
+                    await interaction.channel.send({
+                        content: message,
+                        tts: interaction.options.getBoolean("tts") ?? false,
+                        reply: { messageReference: interaction.options.getString("reply") ?? null }
+                    });
+                }catch (error){
+                    interaction.channel.send({content: `Error sending a message: ${error}`, ephemeral: true})
+                    console.error(error)
+                }
 
                 sentMessage = await interaction.reply({content:"Did it", ephemeral: true});
                 sentMessage.delete()
