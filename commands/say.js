7277da89ba4c9fd65d508c21c69f9e74e4abbc1e
Danrejk <SDanrejk@gmail.com>
2024-05-07 15:45:20 +0200

    added tts option to /say

--- commands/say.js
+++ commands/say.js
@@ -8,10 +8,13 @@ module.exports = {
         .addStringOption(option =>
             option.setName("message").setDescription("message for me to say").setRequired(true)
         )
+        .addBooleanOption(option =>
+            option.setName("tts").setDescription("Text to speech").setRequired(false)
+        )
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
 	execute: async ({ client, interaction }) => {
                 let message = interaction.options.getString("message")
-                await interaction.channel.send({content: message});
+                await interaction.channel.send({content: message, tts: interaction.options.getBoolean("tts") ?? false});
 
                 sentMessage = await interaction.reply({content:"Did it", ephemeral: true});
                 sentMessage.delete()
