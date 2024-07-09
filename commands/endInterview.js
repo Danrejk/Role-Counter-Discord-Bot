768cdfc90df5734602af9fe25478b4869f8440f7
Danrejk <SDanrejk@gmail.com>
2024-07-09 20:03:23 +0200

    Added nationality to /end-interview

--- commands/endInterview.js
+++ commands/endInterview.js
@@ -5,10 +5,12 @@ module.exports = {
 		.setName("end-interview")
 		.setDescription("Send the interview to the admin discord server.")
 		.addStringOption((option) => option.setName("ign").setDescription("in-game nickname").setRequired(true))
+		.addStringOption((option) => option.setName("nationality").setDescription("nationality of the applicant").setRequired(true))
 		.addUserOption((option) => option.setName("user").setDescription("(MANUAL) Applicant account")),
 	execute: async ({client, interaction}) => {
 		// interaction.deferReply();
 
+		// TODO: This should be in it's own separate file
 		// trotu server
 		const interviewerRoleId = "963833565370519632";
 		const applicantRoleId = "1024958417649025116";
@@ -37,20 +39,19 @@ module.exports = {
 			});
 		}
 
-		adminGuild = client.guilds.cache.get(adminGuildId);
+		const adminGuild = client.guilds.cache.get(adminGuildId);
 		if (adminGuild) {
-			interviewsChannel = adminGuild.channels.cache.get(interviewsChannelId);
+			const interviewsChannel = adminGuild.channels.cache.get(interviewsChannelId);
 			if (!interviewsChannel) {
 				await interaction.reply({content: "There has been an error getting the Interviews channel from the Admin discord", ephemeral: true});
 			}
 
-			const ign = interaction.options.getString("ign");
+			// get the applicant
 			user = interaction.options.getUser("user");
 			await interaction.guild.members.fetch();
 
-			// get the applicant
+			// if user is not set manually
 			if (user == null) {
-				// if user is not set manually
 				const threadMembers = await interaction.channel.members;
 				applicant = threadMembers.filter((u) => u.roles.cache.has(applicantRoleId));
 				if (applicant.size != 1) {
@@ -66,19 +67,20 @@ module.exports = {
 			}
 			console.log(user.user);
 
+			const ign = interaction.options.getString("ign");
 			const username = user.user.username;
 			const userId = user.user.id;
-
 			const threadLink = `https://discord.com/channels/${interaction.guild.id}/${interaction.channelId}`;
+			const nationality = interaction.options.getString("nationality");
 
 			// send interview to the admin chat
 			const sentInterview = await interviewsChannel.send({
-				content: `IGN: ${ign}\nUsername: ${username} (${user.user})\nID: ${userId}\n${threadLink}`,
+				content: `IGN: ${ign}\nUsername: ${username} (${user.user})\nID: ${userId}\nNationality: ${nationality}\n${threadLink}`,
 			});
 			const openedThread = await sentInterview.startThread({name: ign});
 			await openedThread.send(`<@&${adminRoleIdAdmin}> <@&${modRoleIdAdmin}> <@&${chieftainRoleIdAdmin}>\n${threadLink}\n`);
 
-			// reply to the interaction
+			// set nickname to the IGN
 			try {
 				await user.setNickname(ign);
 			} catch (error) {
@@ -86,6 +88,7 @@ module.exports = {
 				interaction.channel.send("Failed to set nickname:", error);
 			}
 
+			// reply to the interaction
 			await interaction.reply(`The interview of the user *${user.user} (IGN: ${ign})* has been sent to the Admin Chat!`);
 		}
 	},
