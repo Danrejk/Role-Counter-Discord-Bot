8279dae4bc1bf03229637a9fe70bdbb86d49effe
Danrejk <SDanrejk@gmail.com>
2024-07-14 01:25:29 +0200

    Added basic ability to check the list of linked roles

--- commands/checkLinkedRoles.js
+++ commands/checkLinkedRoles.js
@@ -1,15 +1,26 @@
 const {SlashCommandBuilder} = require("@discordjs/builders");
 const {PermissionFlagsBits} = require("discord-api-types/v10");
+const {statusMessagesLinkedRoles} = require("../components/statusMessageLinkedRoles");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("check-linked-roles")
-		.setDescription("Say stuff")
-		.addStringOption((option) => option.setName("message").setDescription("message for me to say").setRequired(true))
-		.addStringOption((option) => option.setName("reply").setDescription("id of a message to reply").setRequired(false))
-		.addBooleanOption((option) => option.setName("tts").setDescription("Text to speech").setRequired(false))
+		.setDescription("Check list of linked roles.")
+		.addStringOption((option) =>
+			option
+				.setName("type")
+				.setDescription("Type of linked roles for me to display")
+				.setRequired(true)
+				.addChoices(
+					{name: "Member", value: "member", description: "Members of organisations."},
+					{name: "Subject", value: "subject", description: "States under independent countries."}
+				)
+		)
 		.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
 	execute: async ({client, interaction}) => {
-		sentMessage = await interaction.reply({content: "Did it", ephemeral: true});
+		// sentMessage = await interaction.reply({content: "Did it", ephemeral: true});
+		statusMessagesLinkedRoles({client, interactionGuildId: interaction.guild.id}).then(async ({organisations, subjectMasters}) => {
+			await interaction.reply(organisations + subjectMasters);
+		});
 	},
 };
--- components/statusMessageLinkedRoles.js
+++ components/statusMessageLinkedRoles.js
@@ -1,43 +1,79 @@
 const sqlite3 = require("sqlite3").verbose();
 const findRoleEmoji = require("./emoji/findRoleEmoji");
 
-function statusMessages({client, interactionGuildId}) {
+function statusMessagesLinkedRoles({client, interactionGuildId}) {
 	return new Promise((resolve, reject) => {
-		const db = new sqlite3.Database("./data.db", sqlite3.OPEN_READWRITE, (err) => {
+		const db = new sqlite3.Database("./data.db", sqlite3.OPEN_READWRITE, async (err) => {
 			if (err) {
 				console.error(err);
 				reject(err);
 				return;
 			}
 
-			let organisationsMessage = "-------------------------------------------------------------------}\n";
-			let vassalMastersMessage = "-------------------------------------------------------------------}\n";
+			const guild = client.guilds.cache.get(interactionGuildId);
+			await guild.members.fetch();
+			const guildRoles = await guild.roles.fetch();
 
+			let organisationsMessage = "-------------------------------------------------------------------}\n";
+			let subjectMastersMessage = "-------------------------------------------------------------------}\n";
+
+			// LIST OF MASTER ROLES
+			let masterRoles = {};
+			let masterMemberRolesIds = [];
+			let masterSubjectRolesIds = [];
+			db.all(`SELECT DISTINCT masterId, type FROM linkedRoles`, [], (err, results) => {
+				if (err) {
+					console.error(err);
+					reject(err);
+					return;
+				}
+
+				results.forEach((role) => {
+					const guildRole = guildRoles.get(role.masterId);
+					// Member Count
+					const memberCount = guildRole ? guildRole.members.size : 0;
+					// add role to the list
+					masterRoles[role.masterId] = {"memberCount": memberCount, "type": role.type};
+				});
+
+				// Sort by member count
+				let masterRolesSorted = Object.keys(masterRoles);
+				masterRolesSorted.sort((roleIdA, roleIdB) => masterRoles[roleIdB].memberCount - masterRoles[roleIdA].memberCount);
+
+				// Sort into categories
+				masterRolesSorted.forEach((e) => {
+					switch (masterRoles[e].type) {
+						case "member":
+							masterMemberRolesIds.push(e);
+							break;
+						case "subject":
+							masterSubjectRolesIds.push(e);
+							break;
+					}
+				});
+			});
+
+			// LIST OF LINKED ROLES
+			let linkedRoles = {};
+			let linkedMemberRolesIds = [];
+			let linkedSubjectRolesIds = [];
 			db.all(`SELECT * FROM linkedRoles WHERE guildId = ?`, [interactionGuildId], async (err, results) => {
 				if (err) {
 					console.error(err);
 					reject(err);
 					return;
 				}
-				let rolesData = {};
-
-				// fetch needed all data
-				const guild = client.guilds.cache.get(interactionGuildId);
-				await guild.members.fetch();
-
-				const guildRoles = await guild.roles.fetch();
 
 				for (const role of results) {
 					try {
 						const guildRole = guildRoles.get(role.roleId);
 						// Member Count
 						const memberCount = guildRole ? guildRole.members.size : 0;
-
 						// Add role data to rolesData
-						rolesData[role.roleId] = {
+						linkedRoles[role.roleId] = {
+							"masterRole": role.masterId,
+							"type": role.type,
 							"memberCount": memberCount,
-							"category": role.category,
-							"leader": role.leader,
 						};
 					} catch (err) {
 						console.error(err);
@@ -45,81 +81,58 @@ function statusMessages({client, interactionGuildId}) {
 				}
 
 				// Sort by member count
-				let roleIdsSorted = Object.keys(rolesData);
-				roleIdsSorted.sort((roleIdA, roleIdB) => rolesData[roleIdB].memberCount - rolesData[roleIdA].memberCount);
+				let linkedRolesSorted = Object.keys(linkedRoles);
+				linkedRolesSorted.sort((roleIdA, roleIdB) => linkedRoles[roleIdB].memberCount - linkedRoles[roleIdA].memberCount);
 
 				// Sort into categories
-				countryList = [];
-				city_stateList = [];
-				subjectList = [];
-				organisationList = [];
-				religionList = [];
-
-				roleIdsSorted.forEach((e) => {
-					switch (rolesData[e].category) {
-						case "country":
-							countryList.push(e);
-							break;
-						case "city-state":
-							city_stateList.push(e);
+				linkedRolesSorted.forEach((e) => {
+					switch (linkedRoles[e].type) {
+						case "member":
+							linkedMemberRolesIds.push(e);
 							break;
 						case "subject":
-							subjectList.push(e);
-							break;
-						case "organisation":
-							organisationList.push(e);
-							break;
-						case "religion":
-							religionList.push(e);
+							linkedSubjectRolesIds.push(e);
 							break;
 					}
 				});
+			});
 
-				// GENERATE MESSAGE
-				const emojiGuild = client.guilds.cache.get("1259516467721011250");
-				emojiGuild.emojis.cache.clear();
-				await emojiGuild.emojis.fetch();
+			// GENERATE MESSAGE
+			// fetch emojis
+			const emojiGuild = client.guilds.cache.get("1259516467721011250");
+			emojiGuild.emojis.cache.clear();
+			await emojiGuild.emojis.fetch();
 
-				// Countries
-				countriesMessage += "## __Countries__\n";
-				countryList.forEach((e) => {
-					const emoji = findRoleEmoji({client, roleId: e, useEmpty: true});
-					countriesMessage += `- ${emoji} <@&${e}> - ${rolesData[e].leader ?? "*none*"} (${rolesData[e].memberCount})\n`;
-				});
-				// City-States
-				city_statesMessage += "## __City-States__\n";
-				city_stateList.forEach((e) => {
-					const emoji = findRoleEmoji({client, roleId: e, useEmpty: true});
-					city_statesMessage += `- ${emoji} <@&${e}> - ${rolesData[e].leader ?? "*none*"} (${rolesData[e].memberCount})\n`;
-				});
-				// Subjects
-				subjectsMessage += "## __Subjects__\n";
-				subjectList.forEach((e) => {
-					const emoji = findRoleEmoji({client, roleId: e, useEmpty: true});
-					subjectsMessage += `- ${emoji} <@&${e}> - ${rolesData[e].leader ?? "*none*"} (${rolesData[e].memberCount})\n`;
-				});
-				// Organisations
-				organisationsMessage += "## __Organisations__\n";
-				organisationList.forEach((e) => {
-					const emoji = findRoleEmoji({client, roleId: e, useEmpty: true});
-					organisationsMessage += `- ${emoji} <@&${e}> - ${rolesData[e].leader ?? "*none*"} (${rolesData[e].memberCount})\n`;
-				});
-				// Religions
-				religionsMessage += "## __Religions__\n";
-				religionList.forEach((e) => {
-					religionsMessage += `- <@&${e}> - ${rolesData[e].leader ?? "*none*"} (${rolesData[e].memberCount})\n`;
-				});
+			// Organisations and their members
+			organisationsMessage += "## __Members of Organisations__\n";
+			masterMemberRolesIds.forEach((master) => {
+				const masterEmoji = findRoleEmoji({client, roleId: master, useEmpty: false});
+				console.log(linkedMemberRolesIds);
 
-				resolve({
-					countries: countriesMessage,
-					city_states: city_statesMessage,
-					subjects: subjectsMessage,
-					organisations: organisationsMessage,
-					religions: religionsMessage,
+				organisationsMessage += `### __${masterEmoji}<@&${master}> (${masterRoles[master].memberCount}):__\n`;
+				linkedMemberRolesIds.forEach((linked) => {
+					const emoji = findRoleEmoji({client, roleId: linked, useEmpty: true});
+					organisationsMessage += `- ${emoji} <@&${linked}> (${linkedRoles[linked].memberCount})\n`;
 				});
+			});
+			// Subjects and their masters
+			subjectMastersMessage += "## __Subjects__\n";
+			masterSubjectRolesIds.forEach((master) => {
+				const masterEmoji = findRoleEmoji({client, roleId: master, useEmpty: false});
+
+				organisationsMessage += `### __${masterEmoji}<@&${master}> (${masterRoles[master].memberCount}):__\n`;
+				linkedSubjectRolesIds.forEach((linked) => {
+					const emoji = findRoleEmoji({client, roleId: linked, useEmpty: true});
+					organisationsMessage += `- ${emoji} <@&${linked}> (${linkedRoles[linked].memberCount})\n`;
+				});
+			});
+
+			resolve({
+				organisations: organisationsMessage,
+				subjectMasters: subjectMastersMessage,
 			});
 		});
 	});
 }
 
-module.exports = {statusMessages};
+module.exports = {statusMessagesLinkedRoles};
